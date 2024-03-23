6fc215260b0d738974572a7942d071123a74bc1a
Ryan Shepherd <ryanshepherd8492@gmail.com>
2024-03-22 22:32:04 -0700

    Added the get_bet function and the balance checks in main

--- Slotmachine/main.py
+++ Slotmachine/main.py
@@ -1,10 +1,12 @@
 # let's make a slot machine
 
 MAX_LINES = 3
+MAX_BET = 1000
+MIN_BET = 1
 
 def deposit() -> int:
     while True:
-        amount = input("How much will you bet? $")
+        amount = input("How much will you deposit? $")
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
@@ -30,5 +32,33 @@ def get_number_of_lines() -> int:
             print("Please enter a valid number of lines!")
     return lines
 
+def get_bet() -> int:
+    while True:
+        amount = input("What would you like to bet? ")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} and ${MAX_BET}")
+        else:
+            print("Please pick a valid amount to bet.")
+    return amount
+
 def main():
-    pass
+    balance = deposit()
+    lines = get_number_of_lines()
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+        if total_bet > balance:
+            print(f"You do not have enough to bet that amount! Your current balance is ${balance}")
+        else:
+            break
+    
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: {total_bet}.")
+    
+    print(balance, lines, bet)
+
+if __name__ == "__main__":
+    main()
