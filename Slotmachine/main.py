b1dc3a71b3292dab50e0d4625b389330e498657d
Ryan Shepherd <ryanshepherd8492@gmail.com>
2024-03-23 10:57:59 -0700

    Moved the main game to the spin function and trimmed main. Allowed for multiple plays and an exit condition

--- Slotmachine/main.py
+++ Slotmachine/main.py
@@ -52,16 +52,18 @@ def print_slot_machine(columns):
 
 def check_winnings(columns, lines, bet, values):
     winnings = 0
+    winning_lines = []
     for line in range(lines):
-        symbol = colmns[0][line]
+        symbol = columns[0][line]
         for column in columns:
             symbol_to_check = column[line]
             if symbol != symbol_to_check:
                 break
         else:
             winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
 
-    return winnings 
+    return winnings, winning_lines
 
 def deposit() -> int:
     while True:
@@ -104,8 +106,7 @@ def get_bet() -> int:
             print("Please pick a valid amount to bet.")
     return amount
 
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
@@ -115,12 +116,27 @@ def main():
         else:
             break
     
-    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: {total_bet}.")
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}.")
     
     print(balance, lines, bet)
 
     slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
     print_slot_machine(slots)
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
+    print(f"You won ${winnings}.")
+    print(f" You won on lines:", *winning_lines)
+    return winnings - total_bet
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is: ${balance}")
+        answer = input("Press enter to play (q to quit). ")
+        if answer == "q":
+            break
+        balance += spin(balance)
+
+    print(f"You left with ${balance}")
 
 
 if __name__ == "__main__":
